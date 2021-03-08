a7d026f9ad073175cb03c773f23ffea4522f8101
Sharmil <skumar@gomoxie.com>
2021-03-08 11:04:03 +0530

    Script to creat multiple sql servers

---
+++ Azure-SQL instances/main.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:afbeb9ef81dd65ac01106151c6ed4d4d6e6d3cf8c5c778f0195b370241622976
+size 1868
---
+++ Azure-SQL instances/variables.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:00b5ed3d6135b508d5118dee1f975d0cb24edc33e72419f32b9f285fc836418b
+size 120
